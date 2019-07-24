3515cf2d40bc8c4dab78e9c12cb9585441699a69
kevin schuchard <schuchard.kevin@gmail.com>
2019-07-24 08:22:17 -0600

    display values

--- src/app/group-selector/group-selector.component.ts
+++ src/app/group-selector/group-selector.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-group-selector',
   templateUrl: './group-selector.component.html',
   styleUrls: ['./group-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GroupSelectorComponent implements OnInit {
   @Output() changePeople = new EventEmitter<number>();
   @Output() changeGroups = new EventEmitter<number>();
+  people = 4;
+  groups = 2;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.changePeople.next(this.people);
+    this.changeGroups.next(this.groups);
+  }
 
   sliderChange(type, { value }) {
     if (type === 'people') {
+      this.people = value;
       this.changePeople.emit(value);
     }
 
     if (type === 'groups') {
+      this.groups = value;
       this.changeGroups.emit(value);
     }
   }
